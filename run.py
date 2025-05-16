cfc7f1c515189a16b579ed0957e91848219e60b3
AJprogramming123 <147675413+AJprogramming123@users.noreply.github.com>
2025-05-16 12:52:57 -0400

    Update run.py

--- run.py
+++ run.py
@@ -10,4 +10,8 @@ if __name__ == '__main__':
 
 #Testing for productivity
 
-#IF only this changed then the .gititignore works fine now
+#IF only this changed then the .gititignore works fine now after you clone you need to do something specific on your command line:
+#1.) go into the VENV environment always before installing python packages to not conflict your local system
+#2.) Before adding/committing any new updates do this (Only need to this once unless you use a new device or clone again):
+    # git rm --cached .env
+    # git commit -m "Stop tracking .env file"
