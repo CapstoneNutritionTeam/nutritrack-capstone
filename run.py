fbd9125d110bfe48a82fa21d792d2d9b849e1c2e
AJprogramming123 <jaimesandres2023@gmail.com>
2025-05-17 23:25:24 -0400

    MADE THE FINAL COMMENT CHANGES TO GET READY

--- run.py
+++ run.py
@@ -11,10 +11,26 @@ if __name__ == '__main__':
 
 
 #Once you are ready to make yoour first changes--
-#1.) <cd ~/Desktop/nutritrack-capstone> -> <git fetch origin>
-#2.) <git checkout features> -> <git pull origin features> <git checkout -b editor/AJprogramming123>
-#3.) "Start making the changes"
-#4.) <git add .> -> <git commit -m "COMMENT">
-#5.) "You want to make sure youre up to date" -> <git pull origin features>
-#6.) <git add run.py> -> <git commit -m  "Resolve merge conflict"
-#7.) <git push origin editor/AJ123
+#1.) <source venv/bin/activate> -> ssh git@github.com>
+#2.) <cd ~/Desktop/nutritrack-capstone>
+#3.) <git checkout -b editor/AJprogramming123> -> <git pull origin features/[branch]> <- (Creates your own branch)
+#4.) "Start making the changes"
+#5.) <git add run.py> -> <git commit -m  "Resolve merge conflict"
+#6.) <git push origin editor/AJ123> <- this is my personal branch where i make my changes to then send to get pull request to merge with approval)
+
+
+#Basically the most we have to worry about is that when you open a Pull Request and GitHub checks:
+    #"Can this be merged cleanly into features?”
+#If someone else's PR already changed a line you also changed, then GitHub will say:
+    #"This branch has conflicts that must be resolved"
+
+#Now after youre first branch build you can do: <git checkout editor/AJprogramming123> to make sure youre on your branch
+
+
+#What happens when theres conflict code after doing <git pull origin features>?
+# git checkout --ours run.py (Keep their changes) or git checkout --theirs run.py> (Their changes)
+# OR you can just remove these -> (<<<<<<, ========, >>>>>>>>), move the lines you do want to keep or remove and: <git add run.py>
+#Finally: <git commit -m "Merge conflict resolved - kept both changes"
+
+
+#Eventually we will have all branches open for changes so youd use: <git add .> or <git pull origin> instead of specific files or branches
