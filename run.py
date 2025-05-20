b8707c1e60b2c5f7b4633207f9007d11f4cdd12d
AJprogramming123 <147675413+AJprogramming123@users.noreply.github.com>
2025-05-20 18:17:58 -0400

    Testing 1 with my own branch (#9)
    * REsolve it
    
    * MADE THE FINAL COMMENT CHANGES TO GET READY
    
    * MADE THE FINAL COMMENT CHANGES TO GET READY
    
    * Added where the code will go
    
    * added the rest
    
    * Requirement

--- run.py
+++ run.py
@@ -1,19 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
+from app import create_app
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-#Testing for productivity
 
-#IF only this changed then the .gititignore works fine now after you clone you need to do something specific on your command line:
-#1.) go into the VENV environment always before installing python packages to not conflict your local system
-#2.) Before adding/committing any new updates do this (Only need to this once unless you use a new device or clone again):
-    # touch .env
-    # (write something on it to test if it commits)
-    # git rm --cached .env
-    # git commit -m "Stop tracking .env file"
+
