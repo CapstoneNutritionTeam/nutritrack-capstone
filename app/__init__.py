b89e99fd4dc449ed71c6f60b49807a8a4184df4c
AJprogramming123 <147675413+AJprogramming123@users.noreply.github.com>
2025-05-22 16:17:48 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,1 @@
-from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-
-    #Register the Blueprints from the submodules
-    from .searchbar.app import search_bp
-    app.register_blueprint(search_bp, url_prefix='/search')
-
-    return app #Now we set :)
-    
